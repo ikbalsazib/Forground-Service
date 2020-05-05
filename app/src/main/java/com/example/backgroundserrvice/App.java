ca56b2d6a8e13d33375a3bd80b9c61f587afa99d
ikbalsazib <ikbal.sazib@gmail.com>
2020-05-05 15:39:20 +0600

    Forground Service with Notification Builder

---
+++ app/src/main/java/com/example/backgroundserrvice/App.java
@@ -0,0 +1,30 @@
+package com.example.backgroundserrvice;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class App extends Application {
+    public static final String CHANNEL_ID = "exampleServiceChannel";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createNotificationChannel();
+    }
+
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    CHANNEL_ID,
+                    "Example Notification Service",
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(notificationChannel);
+        }
+    }
+}
